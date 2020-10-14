285c1939c9fb0d88cbf4742b0d5c363cebba76ca
Masuda <a.21m.4x@gmail.com>
2020-10-14 07:15:21 +0000

    firstcommit

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,9 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
      Route::get('news/create', 'Admin\NewsController@add');
-     Route::get('news/delete', 'Admin\NewsController@delete');
-     Route::get('news/update', 'Admin\NewsController@update');
+     Route::get('news/edit', 'Admin\NewsController@edit');
+     Route::get('profile/create', 'Admin\ProfileController@add');
+     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
 
  Route :: get('XXX','AAAController@BBB');
