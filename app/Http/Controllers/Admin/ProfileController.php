a02a76ee7974ecc6787753dd9ded12429abd9656
Masuda <a.21m.4x@gmail.com>
2020-10-19 04:49:00 +0000

    Laravel14-1

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,28 +5,45 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class ProfileController extends Controller
+
+use App\profile;
+
+class profileController extends Controller
 {
-    //
-public function add()
-    {
-        return view('admin.profile.create');
-    }
+  public function add()
+  {
+      return view('admin.profile.create');
+  }
 
-    public function create()
-    {
-        return redirect('admin/profile/create');
-    }
+  public function create(Request $request)
+  {
 
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
+      // 以下を追記
+      // Varidationを行う
+      $this->validate($request, profile::$rules);
 
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+      $profile = new profile;
+      $form = $request->all();
+
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $profile->image_path = basename($path);
+      } else {
+          $profile->image_path = null;
+      }
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+
+      return redirect('admin/profile/create');
+  }
 }
 
  
